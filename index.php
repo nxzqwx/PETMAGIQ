3c1148b48233d56a373a44a41eaa402f189a4484
nxzqwx <39726505+nxzqwx@users.noreply.github.com>
2018-06-06 17:15:15 +0200

    index.php
    index stranice

---
+++ index.php
@@ -0,0 +1,106 @@
+
+<section class="section__top">
+    <div class="content">
+<h2>PETMAGIQ VITALITY FORMULA</h2>
+<img src="http://petmagiq.plavipixel.com.hr/files/petmagiq-vitality-formula.png"  alt="petmagiq-vitality-formula">
+<h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h3>
+    </div>
+</section>
+
+<section class="section__tradition">
+    <div class="content">
+<h2>TRADITIONAL USE OF MEDICINAL FUNGI</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+<img src="http://petmagiq.plavipixel.com.hr/files/ganoderma-lucidium.png" alt="ganoderma lucidium">
+<img src="http://petmagiq.plavipixel.com.hr/files/cordyceps-sinesis.png" alt="cordyceps sinesis">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+    </div>
+</section>
+
+<section class="section__profile">
+    <div class="content">
+<h2>THERAPEUTIC PROFILE</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+<p>Lorem ipsum dolor sit amet, ei diceret patrioque euripidis est, eu ius mutat ipsum. Ea vix magna tamquam scripserit. Qui at modus vivendum. Sale audire te usu, vis perfecto inimicus volutpat no, ut nam stet nulla. Vide legendos eu has, ei nam quem oporteat petentium. Delenit inermis te vis, ludus praesent assentior vis eu.</p>
+<img src="http://petmagiq.plavipixel.com.hr/files/therapeutic-left.png" alt="therapeutic-left">
+<img src="http://petmagiq.plavipixel.com.hr/files/therapeutic-right.png" alt="therapeutic-right.png">
+<p>Lorem ipsum dolor sit amet, ei diceret patrioque euripidis est, eu ius mutat ipsum. Ea vix magna tamquam scripserit.</p>
+    </div>
+</section>
+
+<section class="section__philosophy">
+    <div class="content">
+<h2>OUR PHILOSOPHY</h2>
+<img src="http://petmagiq.plavipixel.com.hr/files/our-philosophy.png" alt="philosophy.png">
+<h4>IMMUNITY</h4>
+<p>Natural defense system for healthy life</p>
+<p>Lorem ipsum dolor sit amet, ei diceret patrioque euripidis est, eu ius mutat ipsum. Ea vix magna tamquam scripserit. Qui at modus vivendum. Sale audire te usu, vis perfecto inimicus volutpat no, ut nam stet nulla. Vide legendos eu has, ei nam quem oporteat petentium. Delenit inermis te vis, ludus praesent assentior vis eu.</p>
+<h4>STAMINA</h4>
+<p>Energy for enjoyable and happy life</p>
+<p>Lorem ipsum dolor sit amet, ei diceret patrioque euripidis est, eu ius mutat ipsum. Ea vix magna tamquam scripserit. Qui at modus vivendum. Sale audire te usu, vis perfecto inimicus volutpat no, ut nam stet nulla. Vide legendos eu has, ei nam quem oporteat petentium. Delenit inermis te vis, ludus praesent assentior vis eu.</p>
+<h4>LONGEVITY</h4>
+<p>Ability to live a longer life</p>
+<p>Lorem ipsum dolor sit amet, ei diceret patrioque euripidis est, eu ius mutat ipsum. Ea vix magna tamquam scripserit. Qui at modus vivendum. Sale audire te usu, vis perfecto inimicus volutpat no, ut nam stet nulla. Vide legendos eu has, ei nam quem oporteat petentium. Delenit inermis te vis, ludus praesent assentior vis eu.</p>
+<img src="http://petmagiq.plavipixel.com.hr/files/our-philosophy-bg.png" alt="our-philosophy-bg">
+    </div>
+</section>
+
+<section class="section__product">
+    <div class="content">
+<h2>PRODUCT</h2>
+<img src="http://petmagiq.plavipixel.com.hr/files/product.png" alt="product ">
+<h4>VITALITY FORMULA</h4>
+<p>Lorem ipsum dolor sit amet, ei diceret patrioque euripidis est, eu ius mutat ipsum. Ea vix magna tamquam scripserit.</p>
+<div class="div_top_border_1">
+    </div>
+    <h4>INGRIDIENTS & INSTRUCTIONS</h4>
+    <h4>BIOACTIVE PROFILE</h4>
+<p>Lorem ipsum dolor sit amet, ei diceret patrioque euripidis est, eu ius mutat ipsum. Ea vix magna tamquam scripserit.Lorem ipsum dolor sit amet, ei diceret patrioque euripidis est, eu ius mutat ipsum. Ea vix magna tamquam scripserit.</p>
+    <p>Lorem ipsum dolor sit amet, ei diceret patrioque euripidis est, eu ius mutat ipsum. Ea vix magna tamquam scripserit.Lorem ipsum dolor sit amet, ei diceret patrioque euripidis est, eu ius mutat ipsum. Ea vix magna tamquam scripserit.</p>
+
+<h4>CUSTOM BLENDS</h4>
+<p>Lorem ipsum dolor sit amet, ei diceret patrioque euripidis est, eu ius mutat ipsum. Ea vix magna tamquam scripserit.</p>
+
+<h4>RECOMMENDED DAILY DOSAGE ACCORDING TO ANIMAL WEIGHT: </h4>
+<p>Lorem ipsum dolor sit amet, ei diceret patrioque euripidis est, eu ius mutat ipsum.
+    Ea vix magna tamquam scripserit.Lorem ipsum dolor sit amet, ei diceret patrioque euripidis est, eu ius mutat ipsum.
+    Ea vix magna tamquam scripserit.</p>
+</section>
+
+<section class="section__properties">
+    <div class="content">
+<h4>PROMOTING</h4>
+<p>Lorem ipsum dolor sit amet, ei diceret patrioque euripidis est, eu ius mutat ipsum. Ea vix magna tamquam scripserit.</p>
+<h4>PREVENTIVE & THERAPEUTIC</h4>
+<p>Lorem ipsum dolor sit amet, ei diceret patrioque euripidis est, eu ius mutat ipsum. Ea vix magna tamquam scripserit.</p>
+<h4>RECOVERY</h4>
+<p>Lorem ipsum dolor sit amet, ei diceret patrioque euripidis est, eu ius mutat ipsum. Ea vix magna tamquam scripserit.</p>
+    </div>
+</section>
+
+<section class="section__connect">
+    <div class="content">
+    
+<h2>HAVE A QUESTION ?</h2>
+    <p>Contact us at : info@qip.hr or call us at : +385 1 6040 844</p>
+    <img src="http://petmagiq.plavipixel.com.hr/files/contact.png" alt="contact">
+<h2>GET IN TOUCH</h2>
+    <div class="form">
+    </div>
+</section>
+
+<section class="section__footer">
+    <div class="content">
+<h4>MANUFRACTURER</h4>
+<p>Q inteligentna poljoprivreda d.o.o.
+2. Vrbik 6, 10 000 Zagreb</p>
+<p>T +385 1 6040 844
+INFO@QIP.HR</p>
+<h4>BRAND MANAGER</h4>
+<p>Nenad Fritz</p>
+<p>M+385 98 204804
+NENAD@QIP.HR</p>
+    </div>
+</section>
