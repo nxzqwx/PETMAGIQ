12c04813d6eb7a376ffbe16ff3a2c4b244d6aec4
nxzqwx <39726505+nxzqwx@users.noreply.github.com>
2018-06-07 17:24:44 +0200

    index.php
    Mobile first

--- index.php
+++ index.php
@@ -1,58 +1,193 @@
+<!DOCTYPE html>
+<!-- saved from url=(0034)http://petmagiq.plavipixel.com.hr/ -->
+<html lang="hr"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
+
+
+<!-- Responsive -->
+<meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+<!-- Remove IE's ability to use compatibility mode -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+<!-- Correct type -->
+
+
+<!-- Disable phone formatin on safari -->
+<meta name="format-detection" content="telephone=no">
+
+<!-- Speed up fetching of external assets -->
+<link rel="dns-prefetch" href="http://fonts.googleapis.com/">
+<link rel="dns-prefetch" href="http://ajax.googleapis.com/">
+<link rel="dns-prefetch" href="http://www.google-analytics.com/">
+
+
+<!-- CSS -->
+<link rel="stylesheet" type="text/css" href="./Petmagiq_files/all.css">
+<link rel="stylesheet" type=text/css  href="style.css">
+<link href="./Petmagiq_files/css" rel="stylesheet">
+
+
+
+
+<!-- General -->
+<link rel="shortcut icon" href="http://petmagiq.plavipixel.com.hr/aps/themes/petmagiq/files/favicons/favicon.png">
+<meta name="application-name" content="PETMAGIQ">
+<!-- Chrome -->
+<link rel="icon" type="image/png" sizes="196x196" href="http://petmagiq.plavipixel.com.hr/aps/themes/petmagiq/files/favicons/196.png">
+<link rel="icon" type="image/png" sizes="192x192" href="http://petmagiq.plavipixel.com.hr/aps/themes/petmagiq/files/favicons/192.png">
+<link rel="icon" type="image/png" sizes="128x128" href="http://petmagiq.plavipixel.com.hr/aps/themes/petmagiq/files/favicons/128.png">
+<link rel="icon" type="image/png" sizes="96x96" href="http://petmagiq.plavipixel.com.hr/aps/themes/petmagiq/files/favicons/96.png">
+<link rel="icon" type="image/png" sizes="32x32" href="http://petmagiq.plavipixel.com.hr/aps/themes/petmagiq/files/favicons/32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="http://petmagiq.plavipixel.com.hr/aps/themes/petmagiq/files/favicons/16.png">
+
+<!-- IOS -->
+
+<link rel="apple-touch-icon-precomposed" sizes="152x152" href="http://petmagiq.plavipixel.com.hr/aps/themes/petmagiq/files/favicons/152.png">
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="http://petmagiq.plavipixel.com.hr/aps/themes/petmagiq/files/favicons/144.png">
+<link rel="apple-touch-icon-precomposed" sizes="120x120" href="http://petmagiq.plavipixel.com.hr/aps/themes/petmagiq/files/favicons/120.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="http://petmagiq.plavipixel.com.hr/aps/themes/petmagiq/files/favicons/114.png">
+<link rel="apple-touch-icon-precomposed" sizes="76x76" href="http://petmagiq.plavipixel.com.hr/aps/themes/petmagiq/files/favicons/76.png">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="http://petmagiq.plavipixel.com.hr/aps/themes/petmagiq/files/favicons/72.png">
+<link rel="apple-touch-icon-precomposed" sizes="60x60" href="http://petmagiq.plavipixel.com.hr/aps/themes/petmagiq/files/favicons/60.png">
+<link rel="apple-touch-icon-precomposed" sizes="57x57" href="http://petmagiq.plavipixel.com.hr/aps/themes/petmagiq/files/favicons/57.png">
+
+
+<!-- Win phone -->
+<meta name="msapplication-square310x310logo" content="http://petmagiq.plavipixel.com.hr/aps/themes/petmagiq/files/favicons/310.png">
+<meta name="msapplication-wide310x150logo" content="http://petmagiq.plavipixel.com.hr/aps/themes/petmagiq/files/favicons/310x150.png">
+<meta name="msapplication-square150x150logo" content="http://petmagiq.plavipixel.com.hr/aps/themes/petmagiq/files/favicons/150.png">
+<meta name="msapplication-TileImage" content="http://petmagiq.plavipixel.com.hr/aps/themes/petmagiq/files/favicons/144.png">
+<meta name="msapplication-square70x70logo" content="http://petmagiq.plavipixel.com.hr/aps/themes/petmagiq/files/favicons/70.png">
+
+
+
+<link rel="apple-touch-icon" sizes="180x180" href="http://petmagiq.plavipixel.com.hr/aps/themes/petmagiq/files/favicons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="http://petmagiq.plavipixel.com.hr/aps/themes/petmagiq/files/favicons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="http://petmagiq.plavipixel.com.hr/aps/themes/petmagiq/files/favicons/favicon-16x16.png">
+<link rel="manifest" href="http://petmagiq.plavipixel.com.hr/aps/themes/petmagiq/files/favicons/site.webmanifest">
+<link rel="mask-icon" href="http://petmagiq.plavipixel.com.hr/aps/themes/petmagiq/files/favicons/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#2d89ef">
+<meta name="theme-color" content="#ffffff">
+<!-- Google Analytics -->
+
+<!--END Google Analytics -->
+
+
+<!-- Google Tag Manager -->
+
+<!-- END Google Tag Manager -->
+
+
+<!-- Facebook Pixel Code -->
+
+<!-- End Facebook Pixel Code -->
+<title>Petmagiq</title>
+<meta name="robots" content="noindex,follow">
+<script type="text/javascript">var trackerAjaxUrl="http://petmagiq.plavipixel.com.hr/wp-admin/admin-ajax.php"; var trackerNonce="9e1f03561f";</script><link rel="dns-prefetch" href="http://s.w.org/">
+<script type="text/javascript" src="./Petmagiq_files/jquery.js"></script>
+<script type="text/javascript" src="./Petmagiq_files/jquery-migrate.min.js"></script>
+<script type="text/javascript" src="./Petmagiq_files/tracker.js"></script>
+		<style type="text/css">.recentcomments a{display:inline !important;padding:0 !important;margin:0 !important;}</style>
+		</head>
+
+<body class="home page-template page-template-front-page page-template-front-page-php page page-id-2" cz-shortcut-listen="true">
+
+
+  <header class="top__header">
+
+      <div class="content">
+
+
+    </div>
+
+  </header>
 <section class="section__top">
+<div class="topnav">
+    <div class="topnav__left">
+        <ul>
+<li><a href="tradition">TRADITION</a></li>
+<li><a href="tradition">PROFILE</a></li>
+<li><a href="tradition">PHILOSOPHY</a></li>
+        </ul>
+    </div>
+    <div class="logo">
+    <img id="logo" src="http://petmagiq.plavipixel.com.hr/files/logo.png" alt="logo">
+</div>
+    <div class="topnav__right">
+        <ul>
+<li><a href="tradition">PROPERTIES</a></li>
+<li><a href="tradition">PRODUCT</a></li>
+<li><a href="tradition">CONNECT</a></li>
+        </ul>
+    </div>
+    </div>
     <div class="content">
-<h2>PETMAGIQ VITALITY FORMULA</h2>
-<img src="http://petmagiq.plavipixel.com.hr/files/petmagiq-vitality-formula.png"  alt="petmagiq-vitality-formula">
-<h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h3>
+<h2 id="section__top--h2">PETMAGIQ <br>VITALITY FORMULA</h2>
+<h6>Designed for animals., the PetMagiq Vitality Formula captures the essence of natural therapeutic properties of Reishi and Cordyceps mushrooms.</h6>
+<div class="petmagiq-vitality-formula"><img id="petmagiq-vitality-formula" src="http://petmagiq.plavipixel.com.hr/files/petmagiq-vitality-formula.png"  alt="petmagiq-vitality-formula">
     </div>
 </section>
 
 <section class="section__tradition">
     <div class="content">
-<h2>TRADITIONAL USE OF MEDICINAL FUNGI</h2>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-<img src="http://petmagiq.plavipixel.com.hr/files/ganoderma-lucidium.png" alt="ganoderma lucidium">
-<img src="http://petmagiq.plavipixel.com.hr/files/cordyceps-sinesis.png" alt="cordyceps sinesis">
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+<h2 id="section__tradition--h2">TRADITIONAL USE OF MEDICINAL FUNGI</h2>
+<p id="section__tradition--p">Medicinal mushrooms have been used in the Orient for thousands of years.</p>
+<p id="section__tradition--p">Among many species, Reishi and Cordyceps are the most valued ones. In China, Japan and Korea, Reishi has an ancient reputation as the "Mushroom of Immortality". Cordyceps is another species of medicinal mushrooms, found in high-altitude areas of Tibet.</p>
+</div>
+<div class="section__tradition--ganoderma">
+<img id="ganoderma" src="http://petmagiq.plavipixel.com.hr/files/ganoderma-lucidium.png" alt="ganoderma lucidium">
+<p id="section__tradition--p"> Ganoderma Lucidium</p></div>
+<div class="section__tradition--cordyceps">
+<img id="cordyceps" src="http://petmagiq.plavipixel.com.hr/files/cordyceps-sinesis.png" alt="cordyceps sinesis">
+<p id="section__tradition--p"> Cordyceps Sinesis </p>
+</div>
+<p id="section__tradition--p2">It was described by traditional healers in ancient medicinal scripts as a longevity-promoting remedy. Both fungi have been historically used as a versatile herbal remedies with a unique therapeutic profile.
+The pharmacological and therapeutic properties of medicinal mushrooms are constantly validated through modern science.</p>
     </div>
 </section>
-
 <section class="section__profile">
     <div class="content">
-<h2>THERAPEUTIC PROFILE</h2>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
-<p>Lorem ipsum dolor sit amet, ei diceret patrioque euripidis est, eu ius mutat ipsum. Ea vix magna tamquam scripserit. Qui at modus vivendum. Sale audire te usu, vis perfecto inimicus volutpat no, ut nam stet nulla. Vide legendos eu has, ei nam quem oporteat petentium. Delenit inermis te vis, ludus praesent assentior vis eu.</p>
-<img src="http://petmagiq.plavipixel.com.hr/files/therapeutic-left.png" alt="therapeutic-left">
-<img src="http://petmagiq.plavipixel.com.hr/files/therapeutic-right.png" alt="therapeutic-right.png">
-<p>Lorem ipsum dolor sit amet, ei diceret patrioque euripidis est, eu ius mutat ipsum. Ea vix magna tamquam scripserit.</p>
+<h2 class="section__profile--h2">THERAPEUTIC PROFILE</h2>
+<p id="section__profile--p">More and more veterinarians are taking a holistic approach to the treatment of illness.</p>
+<p id="section__profile--p">Just like humans, animals can suffer from a variety of chronic, degenrative conditions. More and more veterinarians are taking a holistic approach to the treatment of illnesses. That includes looking at various aspects of animal's life, including genetic risk, lifestyle and diet. Only this way, they can find and adress the root causes of their problem.</p>
+<img id="therapeutic-left" src="http://petmagiq.plavipixel.com.hr/files/therapeutic-left.png" alt="therapeutic-left">
+<img id="therapeutic-right" src="http://petmagiq.plavipixel.com.hr/files/therapeutic-right.png" alt="therapeutic-right.png">
+<p id="section__profile--p">Reishi and Cordyceps mushrooms are traditionally used as potent immune stimulators, to combat fatigue, support the respiratory system and liver and to promote longevity.</p>
     </div>
 </section>
 
 <section class="section__philosophy">
     <div class="content">
-<h2>OUR PHILOSOPHY</h2>
+<h2 class="section__philosophy--h2">OUR PHILOSOPHY</h2>
+<div class="philosophy">
 <img src="http://petmagiq.plavipixel.com.hr/files/our-philosophy.png" alt="philosophy.png">
-<h4>IMMUNITY</h4>
-<p>Natural defense system for healthy life</p>
-<p>Lorem ipsum dolor sit amet, ei diceret patrioque euripidis est, eu ius mutat ipsum. Ea vix magna tamquam scripserit. Qui at modus vivendum. Sale audire te usu, vis perfecto inimicus volutpat no, ut nam stet nulla. Vide legendos eu has, ei nam quem oporteat petentium. Delenit inermis te vis, ludus praesent assentior vis eu.</p>
-<h4>STAMINA</h4>
-<p>Energy for enjoyable and happy life</p>
-<p>Lorem ipsum dolor sit amet, ei diceret patrioque euripidis est, eu ius mutat ipsum. Ea vix magna tamquam scripserit. Qui at modus vivendum. Sale audire te usu, vis perfecto inimicus volutpat no, ut nam stet nulla. Vide legendos eu has, ei nam quem oporteat petentium. Delenit inermis te vis, ludus praesent assentior vis eu.</p>
-<h4>LONGEVITY</h4>
-<p>Ability to live a longer life</p>
-<p>Lorem ipsum dolor sit amet, ei diceret patrioque euripidis est, eu ius mutat ipsum. Ea vix magna tamquam scripserit. Qui at modus vivendum. Sale audire te usu, vis perfecto inimicus volutpat no, ut nam stet nulla. Vide legendos eu has, ei nam quem oporteat petentium. Delenit inermis te vis, ludus praesent assentior vis eu.</p>
-<img src="http://petmagiq.plavipixel.com.hr/files/our-philosophy-bg.png" alt="our-philosophy-bg">
+</div>
+<h4 class="section__philosophy--h4">IMMUNITY</h4>
+<p class="section__philosophy--p1">Natural defense system for healthy life</p>
+<p class="section__philosophy--p2">Lorem ipsum dolor sit amet, ei diceret patrioque euripidis est, eu ius mutat ipsum. Ea vix magna tamquam scripserit. Qui at modus vivendum. Sale audire te usu, vis perfecto inimicus volutpat no, ut nam stet nulla. Vide legendos eu has, ei nam quem oporteat petentium. Delenit inermis te vis, ludus praesent assentior vis eu.</p>
+<h4  class="section__philosophy--h4">STAMINA</h4>
+<p class="section__philosophy--p1">Energy for enjoyable and happy life</p>
+<p class="section__philosophy--p2">Lorem ipsum dolor sit amet, ei diceret patrioque euripidis est, eu ius mutat ipsum. Ea vix magna tamquam scripserit. Qui at modus vivendum. Sale audire te usu, vis perfecto inimicus volutpat no, ut nam stet nulla. Vide legendos eu has, ei nam quem oporteat petentium. Delenit inermis te vis, ludus praesent assentior vis eu.</p>
+<h4  class="section__philosophy--h4">LONGEVITY</h4>
+<p class="section__philosophy--p1">Ability to live a longer life</p>
+<p class="section__philosophy--p2">Lorem ipsum dolor sit amet, ei diceret patrioque euripidis est, eu ius mutat ipsum. Ea vix magna tamquam scripserit. Qui at modus vivendum. Sale audire te usu, vis perfecto inimicus volutpat no, ut nam stet nulla. Vide legendos eu has, ei nam quem oporteat petentium. Delenit inermis te vis, ludus praesent assentior vis eu.</p>
+<div class="philosophy">
+<img id="philosophy-bg" src="http://petmagiq.plavipixel.com.hr/files/our-philosophy-bg.png" alt="our-philosophy-bg">
     </div>
 </section>
 
 <section class="section__product">
     <div class="content">
-<h2>PRODUCT</h2>
-<img src="http://petmagiq.plavipixel.com.hr/files/product.png" alt="product ">
-<h4>VITALITY FORMULA</h4>
-<p>Lorem ipsum dolor sit amet, ei diceret patrioque euripidis est, eu ius mutat ipsum. Ea vix magna tamquam scripserit.</p>
+<h2 class="section__product--h2">PRODUCT</h2>
+        <div class="product">
+<img id="productimg" src="http://petmagiq.plavipixel.com.hr/files/product.png" alt="product ">
+</div >
+<h4 class="section__product--h4">VITALITY FORMULA</h4>
+<p class="section__product--p">90 vegan capsules <br> 400mg</p>
+<p class="section__product--p">50% dry cordyceps powder; mycelium and <br> fruiting body ( Cordyceps SInensis )</p>
+<p class="section__product--p">50% reishi powder;mycelium and fruiting <br> body ( Ganoderma Lucidum )</p>
+
 <div class="div_top_border_1">
     </div>
     <h4>INGRIDIENTS & INSTRUCTIONS</h4>
@@ -82,7 +217,7 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor i
 
 <section class="section__connect">
     <div class="content">
-    
+
 <h2>HAVE A QUESTION ?</h2>
     <p>Contact us at : info@qip.hr or call us at : +385 1 6040 844</p>
     <img src="http://petmagiq.plavipixel.com.hr/files/contact.png" alt="contact">
@@ -104,3 +239,5 @@ INFO@QIP.HR</p>
 NENAD@QIP.HR</p>
     </div>
 </section>
+</body>
+</html>
